5c8603a1d1c9ea5d70358ec4db734bede1006871
Jimmy Timmons <hi@jimmy.dev>
2023-09-05 21:09:47 -0400

    chore: update avatar colors logic (#26)

--- components/Common/Avatar.tsx
+++ components/Common/Avatar.tsx
@@ -2,12 +2,35 @@ import { Avatar as MantineAvatar, useMantineColorScheme } from "@mantine/core";
 
 import { FC } from "react";
 
-function getBorderColor(stringInput: string) {
-  let stringUniqueHash = stringInput.split("").reduce((acc, char) => {
-    return char.charCodeAt(0) + ((acc << 5) - acc);
-  }, 0);
-  return `hsl(${stringUniqueHash % 360}, 95%, 35%)`;
-}
+//https://gist.github.com/0x263b/2bdd90886c2036a1ad5bcf06d6e6fb37
+const getStringColor = (
+  string: string,
+  opts?: { hue?: Array<number>; sat?: Array<number>; lit?: Array<number> }
+) => {
+  var h, s, l;
+  opts = opts || {};
+  opts.hue = opts.hue || [0, 360];
+  opts.sat = opts.sat || [75, 100];
+  opts.lit = opts.lit || [40, 60];
+
+  var range = function (hash: number, min: number, max: number) {
+    var diff = max - min;
+    var x = ((hash % diff) + diff) % diff;
+    return x + min;
+  };
+
+  var hash = 0;
+  for (var i = 0; i < string.length; i++) {
+    hash = string.charCodeAt(i) + ((hash << 5) - hash);
+    hash = hash & hash;
+  }
+
+  h = range(hash, opts.hue[0], opts.hue[1]);
+  s = range(hash, opts.sat[0], opts.sat[1]);
+  l = range(hash, opts.lit[0], opts.lit[1]);
+
+  return `hsl(${h}, ${s}%, ${l}%)`;
+};
 
 interface AvatarProps {
   name: string;
@@ -17,22 +40,24 @@ interface AvatarProps {
 const Avatar: FC<AvatarProps> = ({ name, size }) => {
   const { colorScheme } = useMantineColorScheme();
   const initial = (name[0] || "u").toUpperCase();
+  const color = getStringColor(name, {
+    hue: [225, 350],
+    sat: [40, 70],
+  });
 
   return (
     <MantineAvatar
       radius="xl"
-      variant="gradient"
       size={size || 32}
-      gradient={{
-        from: getBorderColor(name),
-        to: colorScheme === "light" ? "#fff" : "#000",
-        deg: 145,
-      }}
-      sx={{
+      sx={({ colors }) => ({
         "& >.mantine-Avatar-placeholder": {
-          border: `3px solid ${getBorderColor(name)}`,
+          backgroundColor: color,
+          border: `1px solid ${
+            colorScheme === "dark" ? colors.gray[5] : colors.gray[2]
+          }`,
+          color: "#FFF",
         },
-      }}
+      })}
     >
       {initial}
     </MantineAvatar>
